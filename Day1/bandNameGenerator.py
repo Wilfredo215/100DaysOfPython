2364caeaad803c97c3563fb9067a5ef0c9fd8517
wjlopez_spawar <wilfredo.j.lopez2.ctr@us.navy.mil>
2024-12-17 14:53:37 -0500

    employee traveled time

--- Day1/bandNameGenerator.py
+++
@@ -1,5 +0,0 @@
-city_Name = input(str("What is your city name: "))
-pet_Name = input(str("What is your pet's name: "))
-
-
-print("Your band name could be " + city_Name + " " + pet_Name)
---
+++ Variables/employeeTravel.py
@@ -0,0 +1,11 @@
+employeeA = 15.62
+employeeB = 41.85
+employeeC = 32.67
+
+a = float(input("a miles: "))
+b = float(input("b miles: "))
+c = float(input("c miles: "))
+
+total_miles_tranveled = (employeeA * a) + (employeeB * b) + (employeeC * c)
+
+print(f"Distance: {total_miles_tranveled}")
